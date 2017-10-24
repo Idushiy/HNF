de0fdda9ed7eecbd89387827e34d8d6a147405fc
Andrey <matradze@gmail.com>
2017-10-24 08:54:45 +0700

    webbuild 24.10

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//= ../libs/jquery/dist/jquery.js
+//= ../libs/popper.js/dist/umd/popper.js
+//= ../libs/bootstrap/js/dist/util.js
+//= ../libs/bootstrap/js/dist/tooltip.js
+//= ../libs/bootstrap/js/dist/popover.js
+//= custom/custom.js
