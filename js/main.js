f39e81cd801911c7962d57ad24e7c31ada6e4217
Andrey <matradze@gmail.com>
2017-10-27 09:53:25 +0700

    day 11

--- js/main.js
+++
@@ -1,6 +0,0 @@
-//= ../libs/jquery/dist/jquery.js
-//= ../libs/popper.js/dist/umd/popper.js
-//= ../libs/bootstrap/js/dist/util.js
-//= ../libs/bootstrap/js/dist/tooltip.js
-//= ../libs/bootstrap/js/dist/popover.js
-//= custom/custom.js
